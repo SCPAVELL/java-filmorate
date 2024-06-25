70cd9c149556b9d42977b43f8e7aeef8e0120f2c
Pavel <pavel2001stepanov@yandex.ru>
2024-06-25 19:49:55 +0300

    FIX CHECKSTYLE

--- src/main/java/ru/yandex/practicum/filmorate/model/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/service/UserService.java
@@ -1,14 +1,11 @@
 package ru.yandex.practicum.filmorate.model.service;
 
 import lombok.AllArgsConstructor;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.CustomValidationException;
-import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
-
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
