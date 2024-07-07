6c538df244c398ec41c89fc9c6217bde2c53bf78
Pavel <pavel2001stepanov@yandex.ru>
2024-07-07 22:19:27 +0300

    exception handling  FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,6 +2,8 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
+import ru.yandex.practicum.filmorate.exception.FilmValidationException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.service.FilmService;
 import java.util.Collection;
@@ -48,15 +50,21 @@ public class FilmController {
 	}
 
 	@PostMapping
-	public Film create(@RequestBody Film film) {
+	public ResponseEntity<Film> create(@RequestBody Film film) {
 		log.info("Получен запрос POST. Данные тела запроса: {}", film);
 		try {
 			Film validFilm = filmService.add(film);
 			log.info("Создан объект {} с идентификатором {}", Film.class.getSimpleName(), validFilm.getId());
-			return validFilm;
-		} catch (ResponseStatusException e) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(validFilm);
+		} catch (NotFoundException e) {
 			log.error("Ошибка при создании фильма: {}", e.getMessage());
-			throw e;
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+		} catch (FilmValidationException e) {
+			log.error("Ошибка валидации фильма: {}", e.getMessage());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+		} catch (Exception e) {
+			log.error("Произошла ошибка при создании фильма: {}", e.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
 		}
 	}
 
