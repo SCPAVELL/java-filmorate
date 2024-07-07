54df7d38dc7bddb50cc79e72abf9f6c149461178
Pavel <pavel2001stepanov@yandex.ru>
2024-07-07 20:57:53 +0300

    mpa processing with a non-existent indicator in the Mpa Controller and GenreController classes

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -2,10 +2,14 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.service.GenreService;
 import java.util.Collection;
@@ -30,6 +34,10 @@ public class GenreController {
 	@GetMapping("/{id}")
 	public Genre findGenre(@PathVariable String id) {
 		log.info("Получен запрос GET к эндпоинту: /genres/{}", id);
-		return genreService.getGenre(id);
+		try {
+			return genreService.getGenre(id);
+		} catch (NotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+		}
 	}
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -3,10 +3,14 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.model.service.MpaService;
 
@@ -32,6 +36,10 @@ public class MpaController {
 	@GetMapping("/{id}")
 	public Mpa findGenre(@PathVariable String id) {
 		log.info("Получен запрос GET к эндпоинту: /mpa/{}", id);
-		return mpaService.getMpa(id);
+		try {
+			return mpaService.getMpa(id);
+		} catch (NotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+		}
 	}
 }
