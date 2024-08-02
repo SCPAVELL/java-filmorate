37616ba6d4149067f6f43acab90ee1a8aae9236f
Pavel <pavel2001stepanov@yandex.ru>
2024-08-02 17:48:18 +0300

    fix checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/enums/Search.java
+++ src/main/java/ru/yandex/practicum/filmorate/enums/Search.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public enum Search {
+
 	title("title"), director("director"), titleAndDirector("title,director"), directorAndTitle("director,title");
 
 	private static Map<String, Search> elements;
@@ -12,6 +13,7 @@ public enum Search {
 	static {
 		elements = Arrays.stream(values()).collect(Collectors.toMap(e -> e.by, e -> e));
 	}
+
 	private String by;
 
 	Search(String by) {
