8b6d576c927ae32642180e580b6e13da3df127b5
Pavel <pavel2001stepanov@yandex.ru>
2024-08-06 14:47:03 +0300

    fix checkstyle DirectorController

--- src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.service.DirectorService;
